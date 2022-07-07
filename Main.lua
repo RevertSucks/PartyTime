d1d889c477dd1f69b5a65c4f63bc74fcd86fa5a4
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-07-07 16:57:13 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,25 +1,20 @@
-local gameIds = {
-    ["Build A Boat"] = 537413528,
-}
+local gameIds = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/PartyTime/main/Supported.lua"))
 local foundHit
 
 local function import(file)
     loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/RevertSucks/PartyTime/main/scripts/%s.lua"):format(file)),file..'.lua')()
 end
 
-local function load()
-    for i,v in pairs(gameIds) do
-        if game.PlaceId == v then
-            print(i)
-            foundHit = string.gsub(i," ","%%20")
-            print(foundHit)
-        end
-    end
-    if foundHit ~= nil then
-        import(foundHit)
-    else
-        import("Universal")
+for i,v in pairs(gameIds) do
+    if game.PlaceId == v then
+        print(i)
+        foundHit = string.gsub(i," ","%%20")
+        print(foundHit)
     end
 end
 
-load()
+if foundHit ~= nil then
+    import(foundHit)
+else
+    import("Universal")
+end
