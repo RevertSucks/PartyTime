d68fae538172b5ef5dfb2559579e13f62733c23f
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-07-07 16:30:43 -0500

    Create Hydroxide.lua

---
+++ archives/Hydroxide.lua
@@ -0,0 +1,124 @@
+--original upload: https://github.com/Upbolt/Hydroxide/blob/revision/ui/main.lua
+--[[
+local owner = "Upbolt"
+local branch = "revision"
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+webImport("init")
+webImport("ui/main")
+]]--
+
+local CoreGui = game:GetService("CoreGui")
+local UserInput = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
+
+local Interface = import("rbxassetid://5042109928")
+
+if oh.Cache["ui/main"] then
+	return Interface
+end
+
+import("ui/controls/TabSelector")
+local MessageBox, MessageType = import("ui/controls/MessageBox")
+
+local RemoteSpy
+local ClosureSpy
+local ScriptScanner
+local ModuleScanner
+local UpvalueScanner
+local ConstantScanner
+
+xpcall(function()
+	RemoteSpy = import("ui/modules/RemoteSpy")
+	ClosureSpy = import("ui/modules/ClosureSpy")
+	ScriptScanner = import("ui/modules/ScriptScanner")
+	ModuleScanner = import("ui/modules/ModuleScanner")
+	UpvalueScanner = import("ui/modules/UpvalueScanner")
+	ConstantScanner = import("ui/modules/ConstantScanner")
+end, function(err)
+	local message
+	if err:find("valid member") then
+		message = "The UI has updated, please rejoin and restart. If you get this message more than once, screenshot this message and report it in the Hydroxide server.\n\n" .. err
+	else
+		message = "Report this error in Hydroxide's server:\n\n" .. err
+	end
+
+	MessageBox.Show("An error has occurred", message, MessageType.OK, function()
+		Interface:Destroy() 
+	end)
+end)
+
+local constants = {
+	opened = UDim2.new(0.5, -325, 0.5, -175),
+	closed = UDim2.new(0.5, -325, 0, -400),
+	reveal = UDim2.new(0.5, -15, 0, 20),
+	conceal = UDim2.new(0.5, -15, 0, -75)
+}
+
+local Open = Interface.Open
+local Base = Interface.Base
+local Drag = Base.Drag
+local Status = Base.Status
+local Collapse = Drag.Collapse
+
+function oh.setStatus(text)
+	Status.Text = '• Status: ' .. text
+end
+
+function oh.getStatus()
+	return Status.Text:gsub('• Status: ', '')
+end
+
+local dragging
+local dragStart
+local startPos
+
+Drag.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		local dragEnded 
+
+		dragging = true
+		dragStart = input.Position
+		startPos = Base.Position
+
+		dragEnded = input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+				dragEnded:Disconnect()
+			end
+		end)
+	end
+end)
+
+oh.Events.Drag = UserInput.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement and dragging then
+		local delta = input.Position - dragStart
+		Base.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+end)
+
+Open.MouseButton1Click:Connect(function()
+	Open:TweenPosition(constants.conceal, "Out", "Quad", 0.15)
+	Base:TweenPosition(constants.opened, "Out", "Quad", 0.15)
+end)
+
+Collapse.MouseButton1Click:Connect(function()
+	Base:TweenPosition(constants.closed, "Out", "Quad", 0.15)
+	Open:TweenPosition(constants.reveal, "Out", "Quad", 0.15)
+end)
+
+MessageBox.Show("Welcome to Hydroxide", "This is not a finished product\n\nUPDATE: 1/8/22\nMy Discord account has been terminated, which means the status of the current Discord server is now in limbo. I have no plans to create a new server right now, but I do plan to in the near future.", MessageType.OK)
+
+Interface.Name = HttpService:GenerateGUID(false)
+if getHui then
+	Interface.Parent = getHui()
+else
+	if syn then
+		syn.protect_gui(Interface)
+	end
+
+	Interface.Parent = CoreGui
+end
+
+return Interface
